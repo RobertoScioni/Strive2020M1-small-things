9c2005547ea7bdf31d26181440e6fbaf4927aefa
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-10-01 14:51:48 +0200

    excercise 4 &5 solved

--- M1-D4/M1-D4.js
+++ M1-D4/M1-D4.js
@@ -10,13 +10,13 @@ ASSIGNMENT RULES
 Write a function "area" which receives 2 parameters (l1,l2) and calculate the area of the rectangle.
 */
     console.log("exercise1")
-    let area=(l1,l2)=>l1*l2
+    const area=(l1,l2)=>l1*l2
     console.log("   Area of a rectangle "+area(5,3))
 /* EXERCISE 2
 Write a function "crazySum" which receives two given integers. If the two values are same, then returns triple their sum.
 */
     console.log("exercise2")
-    let crazySum=(one,two)=>one===two?3*(one+two):""
+    const crazySum=(one,two)=>one===two?3*(one+two):""
     console.log("   the crazy sum of 5 and 5 is"+crazySum(5,5))
     console.log("   the crazy sum of 5 and 2 is"+crazySum(5,2))
 /* EXERCISE 3
@@ -27,21 +27,31 @@ the specified
 number is greater than 19.
 */
     console.log("exercise3")
-    let crazyDiff=(one)=> one>19?3*Math.abs(19-one):""
+    const crazyDiff=(one)=> one>19?3*Math.abs(19-one):""
     console.log("   the crazy diff of 20 is "+crazyDiff(20))
     console.log("   the crazy sum of 17 is "+crazyDiff(17))
 /* EXERCISE 4
 Write a function "boundary" which accept an integer N and returns true if N is within 20 and 100 (included) or equal to 400.
-*/
-
+*/   
+  
 /* WRITE YOUR CODE HERE */
-
+console.log("exercise4")
+    const boundary=(one)=>one===400||(20<=one&&one<=100)?true:false
+    
+    console.log("   "+boundary(400))
+    console.log("   "+boundary(70))
+    console.log("   "+boundary(101))
+    console.log("   "+boundary(15))
 /* EXERCISE 5
 Write a function "strivify" which accepts a string S. Add to S "Strive" in front of a given string, if the given string begins with "Strive" then return the original string.
 */
 
 /* WRITE YOUR CODE HERE */
+console.log("exercise5")
+    const strivify=(S)=>S.startsWith("Strive")?S:S="Strive"+S
 
+    console.log("   "+strivify("Roberto"))
+    console.log("   "+strivify("Strive school"))
 /* EXERCISE 6
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
